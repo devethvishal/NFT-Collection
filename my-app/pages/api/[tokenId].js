34b0497b3188588c70de59fcaa800c9e6c4d6054
Vishal Srivastava <eth.vishal@gmail.com>
2022-06-06 12:39:41 +0530

    Updated the [tokenId].js

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -2,7 +2,11 @@
 
 export default function handler(req, res) {
   const tokenId = req.query.tokenId;
-  const imageURL = ""
+  const imageURL = "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
 
-  res.status(200).json({ name: 'Vishal Srivastava' })
+  res.status(200).json({
+    name: "Crypto Dev #" + tokenId,
+    description: "Crypto Dev is a collection of developers in crypto",
+    image: imageURL + tokenId + ".svg",
+  })
 }
